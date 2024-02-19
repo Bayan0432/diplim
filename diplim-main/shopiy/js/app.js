37535797e037d9ff3220bbe31f1d4aec853c5162
Bayan0432 <130027518+Bayan0432@users.noreply.github.com>
2024-02-19 18:09:30 +0600

    Update app.js

--- diplim-main/shopiy/js/app.js
+++ diplim-main/shopiy/js/app.js
@@ -18,38 +18,47 @@ document.addEventListener('mousedown', function(e){
   }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  var form = document.getElementById('myForm');
-  var nameInput = document.getElementById('nameInput');
-  var emailInput = document.getElementById('emailInput');
-  var phoneInput = document.getElementById('phoneInput');
 
-  form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent the form from submitting
+
+
+
+//document.addEventListener('DOMContentLoaded', function() {
+//  var form = document.getElementById('myForm');
+//  var nameInput = document.getElementById('nameInput');
+//  var emailInput = document.getElementById('emailInput');
+//var phoneInput = document.getElementById('phoneInput');
+
+//  form.addEventListener('submit', function(event) {
+ //     event.preventDefault(); // Prevent the form from submitting
 
       // Check if all fields are filled
-      if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || phoneInput.value.trim() === '') {
-          alert('Пожалуйста, заполните все поля.');
-          return;
-      }
+ //     if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || phoneInput.value.trim() === '') {
+ //         alert('Пожалуйста, заполните все поля.');
+   //       return;
+//}
 
       // Check email format
-      var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailPattern.test(emailInput.value)) {
-          alert('Пожалуйста, введите корректный адрес электронной почты.');
-          return;
-      }
+//      var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//      if (!emailPattern.test(emailInput.value)) {
+//          alert('Пожалуйста, введите корректный адрес электронной почты.');
+ //         return;
+ //     }
 
-      // Check phone number format
-      var phonePattern = /^\d{10}$/;
-      if (!phonePattern.test(phoneInput.value)) {
-          alert('Пожалуйста, введите корректный номер телефона (10 цифр без разделителей).');
-          return;
-      }
+ //     // Check phone number format
+//      var phonePattern = /^\d{10}$/;
+ //     if (!phonePattern.test(phoneInput.value)) {
+ //         alert('Пожалуйста, введите корректный номер телефона (10 цифр без разделителей).');
+ //         return;
+ //     }
 
       // If all validations pass, show success message
-      alert('Ваша заявка успешно отправлена !');
-      form.reset();
-  });
-});
+//      alert('Ваша заявка успешно отправлена !');
+ //     form.reset();
+ // });
+//}); 
 
+
+
+//----------Модальное окно -------//
+
+					
